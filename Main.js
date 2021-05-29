a7d61a2665aefbecca6b8a73913e29a08732825d
cs17jjc <quantumphysicslhc.cuth@gmail.com>
2021-05-29 13:53:21 +0100

    init

--- Main.js
+++ Main.js
@@ -23,29 +23,16 @@ document.addEventListener('keyup', (e) => {
 let mySongData = zzfxM(...bgMusicSong);
 let myAudioNode = zzfxP(...mySongData);
 myAudioNode.loop = true;
-myAudioNode.start();
+//myAudioNode.start();
 var musicToggle = true;
 var soundToggle = true;
-
-var paused = false;
-
-var playing = false;
 
 
 function draw(ctx) {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    if (!this.paused && !gameState.gameOver && (!gameState.endGame || (Date.now() - gameState.endGameTimer) < 3000)) {
-        gameState.update(inputs.getInputs(), soundToggle);
-    }
-    ctx.save();
+    gameState.update(inputs.getInputs(), soundToggle);
     gameState.draw(ctx);
-    ctx.restore();
-    if (this.paused) {
-        ctx.textAlign = 'left'
-        ctx.font = "99px Courier New";
-        ctx.fillStyle = rgbToHex(250, 250, 250);
-        ctx.fillText("PAUSED", canvasWidth * 0.32, canvasHeight * 0.4);
-    }
+
     ctx.textAlign = 'left'
     ctx.font = "15px Courier New";
     ctx.fillStyle = rgbToHex(0, 0, 0);
@@ -57,30 +44,6 @@ function draw(ctx) {
     if (musicToggle) {
         ctx.font = "40px Courier New";
         ctx.fillText("♬", canvasWidth * 0.05, canvasHeight * 0.98);
-    }
-
-    if (!inputs.prevStates.includes("ESC") && inputs.getInputs().includes("ESC")) {
-        if (gameState.inShop) {
-            gameState.inShop = false;
-            gameState.shopCutscene = false;
-        } else {
-            if (!gameState.spawnBoss) {
-                this.paused = !this.paused;
-            }
-        }
-    }
-
-    if (!inputs.prevStates.includes("RESTART") && inputs.getInputs().includes("RESTART")) {
-        var coins = localStorage.getItem("AJSNDJNSAJKJNDSKJMirroriaCoinsYRYRBHJASKWA");
-        var weapon = JSON.parse(localStorage.getItem("AJSNDJNSAJKJNDSKJMirroriaWeaponYRYRBHJASKWA"));
-        gameState = GameState.initial(coins, weapon, gameState.lastShopNum);
-    }
-
-    if (!inputs.prevStates.includes("CLEARSTORAGE") && inputs.getInputs().includes("CLEARSTORAGE")) {
-        gameState.coins = 0;
-        gameState.equipedWeapon = makeStartWeapon(gameState.playerSize);
-        localStorage.setItem("AJSNDJNSAJKJNDSKJMirroriaCoinsYRYRBHJASKWA", 0);
-        localStorage.setItem("AJSNDJNSAJKJNDSKJMirroriaWeaponYRYRBHJASKWA", JSON.stringify(gameState.equipedWeapon));
     }
 
     if (!inputs.prevStates.includes("MUTE") && inputs.getInputs().includes("MUTE")) {
@@ -105,51 +68,6 @@ function draw(ctx) {
         }
     }
 
-    if (!playing) {
-        ctx.fillStyle = rgbToHexAlpha(0, 0, 0, 220);
-        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-        var scale = 5;
-        ctx.drawImage(textures.get("MirroriaImg"), (canvasWidth / 2) - ((118 * scale) / 2), canvasHeight * 0.05, 118 * scale, 33 * scale);
-        ctx.fillStyle = rgbToHex(250, 250, 250);
-        ctx.font = "20px Courier New";
-        ctx.textAlign = 'center';
-        ctx.fillText("WASD to Move.", canvasWidth / 2, canvasHeight * 0.45);
-        ctx.fillText("Left and Right Arrow Keys to Attack.", canvasWidth / 2, canvasHeight * 0.55);
-        ctx.fillText("Q and E to Switch Item.", canvasWidth / 2, canvasHeight * 0.65);
-        ctx.fillText("Shift to Use Item.", canvasWidth / 2, canvasHeight * 0.75);
-        ctx.font = "24px Courier New";
-        ctx.fillText("Press Enter to Start.", canvasWidth / 2, canvasHeight * 0.85);
-        if (inputs.getInputs().includes("ENTER") && !inputs.prevStates.includes("ENTER")) {
-            playing = true;
-            inputs.attachInputs();
-        }
-    }
-    if (gameState.endGame && (Date.now() - gameState.endGameTimer) > 5000) {
-        ctx.drawImage(textures.get("EndImg"), 0, 0);
-        var ratio = Math.min(1, ((Date.now() - gameState.endGameTimer) - 5000) / 1000);
-        ctx.fillStyle = rgbToHexAlpha(250, 250, 250, Math.trunc(255 * ratio));
-        ctx.font = "25px Courier New";
-        ctx.textAlign = 'center';
-        ctx.fillText("Thanks For Playing!", canvasWidth * 0.52, canvasHeight * 0.5 - (50 * ratio));
-        ctx.font = "20px Courier New";
-        ctx.fillText("Press Enter to Replay.", canvasWidth * 0.52, canvasHeight * 0.58 - (50 * ratio));
-        ctx.fillStyle = rgbToHex(29, 161, 242);
-        ctx.fillText("Press E to Tweet score.", canvasWidth * 0.52, canvasHeight * 0.64 - (50 * ratio));
-        ctx.font = "15px Courier New";
-        ctx.fillText("This will open a popup.", canvasWidth * 0.52, canvasHeight * 0.67 - (50 * ratio));
-        if (inputs.getInputs().includes("ENTER") && !inputs.prevStates.includes("ENTER")) {
-            playing = false;
-            inputs = Inputs.empty();
-            inputs.attachInput("ENTER", 'Enter'.toLowerCase());
-            var coins = localStorage.getItem("AJSNDJNSAJKJNDSKJMirroriaCoinsYRYRBHJASKWA");
-            var weapon = JSON.parse(localStorage.getItem("AJSNDJNSAJKJNDSKJMirroriaWeaponYRYRBHJASKWA"));
-            gameState = GameState.initial(coins, weapon, -1);
-        }
-        if(inputs.getInputs().includes("NEXTITEM") && !inputs.prevStates.includes("NEXTITEM")){
-            tweetFinished(gameState.coins);
-        }
-    }
-
     inputs.prevStates = inputs.getInputs();
 }
 
@@ -158,9 +76,7 @@ var loadChecker;
 loadChecker = setInterval(() => {
     if (loadedImages) {
         clearInterval(loadChecker);
-        var coins = localStorage.getItem("AJSNDJNSAJKJNDSKJMirroriaCoinsYRYRBHJASKWA");
-        var weapon = JSON.parse(localStorage.getItem("AJSNDJNSAJKJNDSKJMirroriaWeaponYRYRBHJASKWA"));
-        gameState = GameState.initial(coins, weapon, -1);
+        gameState = GameState.initial();
         setInterval(() => draw(ctx), 50);
     } else {
         loadedImages = true;
