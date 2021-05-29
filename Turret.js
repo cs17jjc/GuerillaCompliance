66f10696d75b1aa8ac092e5e54390edb0e890bfe
cs17jjc <quantumphysicslhc.cuth@gmail.com>
2021-05-29 21:03:28 +0100

    Fixed typo

--- Turret.js
+++ Turret.js
@@ -1,19 +1,18 @@
 ///Parent class for turrets
 ///type provides type of turret, which should contain 'damage', 'firingSpeed', and 'bulletSpeed'
-class Turret{
-    constructor(type,damage,firingSpeed,bulletSpeed,accuracy,range)
-    {
+class Turret {
+    constructor(type, damage, firingSpeed, bulletSpeed, accuracy, range) {
         this.type = type
         this.damage = damage
         this.firingSpeed = firingSpeed
         this.bulletSpeed = bulletSpeed
         this.accuracy = accuracy
-        this.range = rage;
+        this.range = range;
     }
-    static standardTurret(){
-        return new Turret("STANDARD",1,50,1,0.8,20);
+    static standardTurret() {
+        return new Turret("STANDARD", 1, 50, 1, 0.8, 20);
     }
-    static sniperTurret(){
-        return new Turret("SNIPER",2,150,2,1,50);
+    static sniperTurret() {
+        return new Turret("SNIPER", 2, 150, 2, 1, 50);
     }
 }
